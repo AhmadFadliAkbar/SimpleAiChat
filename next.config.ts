97a5e83b6d7ae7cef7f9e71c01547b78a43abab9
Rafieaydin <rafie.aydin1@gmail.com>
2025-06-14 11:41:31 +0700

    test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Ganti kembali ke 'standalone' untuk kebutuhan Docker
-  output: 'standalone',
+  output: process.env.NEXT_OUTPUT as 'standalone' | 'export' | undefined || 'standalone',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
