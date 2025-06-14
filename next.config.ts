080f153ab4fe5080808604d36f982e5fe1a5ab9b
Rafieaydin <rafie.aydin1@gmail.com>
2025-06-14 13:15:18 +0700

    final test

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // ini buat exprt gihthub action
   output: process.env.NEXT_OUTPUT as 'standalone' | 'export' | undefined || 'standalone',
   images: {
     unoptimized: true, // Diperlukan untuk static export
   },
-  // Gunakan nama repository asli
+  // Gunakan nama repository asli buat export github action
   basePath: process.env.NEXT_OUTPUT === 'export' ? '/SimpleAiChat' : '',
   assetPrefix: process.env.NEXT_OUTPUT === 'export' ? '/SimpleAiChat/' : '',
 };
