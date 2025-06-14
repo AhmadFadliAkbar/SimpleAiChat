90e0f456ee134ae5ddad532d04150d1ca9e50b80
Rafieaydin <rafie.aydin1@gmail.com>
2025-06-14 11:25:47 +0700

    feat: Add GitHub Pages deployment to main branch

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Ini instruksi utama untuk menghasilkan output statis
+import type { NextConfig } from "next";
 
-  // OPSIONAL: Konfigurasi basePath dan assetPrefix
-  // Gunakan ini jika situs GitHub Pages Anda di-host di sub-jalur,
-  // misalnya: https://<USERNAME>.github.io/<NAMA_REPOSITORY_ANDA>/
-  // GANTI <NAMA_REPOSITORY_ANDA> dengan nama repositori Anda.
-  // Contoh: jika repo Anda 'my-app', maka '/my-app'
-  // basePath: '/<NAMA_REPOSITORY_ANDA>',
-  // assetPrefix: '/<NAMA_REPOSITORY_ANDA>/',
-
-  // Direkomendasikan untuk export statis jika Anda menggunakan komponen Image dari Next.js
-  images: {
-    unoptimized: true,
-  },
+const nextConfig: NextConfig = {
+  // Ganti kembali ke 'standalone' untuk kebutuhan Docker
+  output: 'export',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
